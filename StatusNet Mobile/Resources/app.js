0ad4ece179714e4945f18b65294a374d84634cab
Zach Copley <zach@status.net>
2010-03-29 19:45:58 -0700

    Some changes to get mobile to build

--- StatusNet Mobile/Resources/app.js
+++ StatusNet Mobile/Resources/app.js
@@ -1,9 +1,12 @@
+// StatusNet
+
 // this sets the background color of the master UIView (when there are no windows/tab groups on it)
 Titanium.UI.setBackgroundColor('#000');
 
 // create tab group
 var tabGroup = Titanium.UI.createTabGroup();
 
+var tabGroup2 = null;
 
 //
 // create base UI tab and root window
