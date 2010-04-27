df97e6bbe654a8bf53d12f0607542eeb30bac52c
Brion Vibber <brion@pobox.com>
2010-04-26 17:19:04 -0700

    prettify a bit; avatars loading, working on the dialog model

--- StatusNet Desktop/Resources/view/statusnet_settingsview.js
+++ StatusNet Desktop/Resources/view/statusnet_settingsview.js
@@ -1,15 +1,9 @@
 StatusNet.SettingsView = function() {
+	var db = StatusNet.getDB();
+	this.accounts = StatusNet.Account.listAll(db);
 }
 StatusNet.SettingsView.prototype.init = function() {
-	var db = StatusNet.getDB();
-	var accounts = StatusNet.Account.listAll(db);
-	var bits = '';
-	for (var i = 0; i < accounts.length; i++) {
-		var ac = accounts[i];
-		bits += '<tr><td>' + ac.username + '</td><td>' + ac.apiroot + '</td></tr>';
-	}
-	StatusNet.debug('HEY GUYS' + bits);
-	$("#accountlist").html(bits);
+	this.showAccounts();
 
 	$("#button_addaccount").click(function() {
 
@@ -27,4 +21,44 @@ StatusNet.SettingsView.prototype.init = function() {
 			  snc = new StatusNet.Client(ac);
 		 }
 	});
+}
+
+StatusNet.SettingsView.prototype.showAccounts = function() {
+	var list = $('#accountlist tbody')[0];
+	for (var i = 0; i < this.accounts.length; i++) {
+		StatusNet.debug('hi');
+
+		var acct = this.accounts[i];
+		var tr = document.createElement('tr');
+
+		StatusNet.debug('x1');
+		var td_icon = document.createElement('td');
+		var img_icon = document.createElement('img');
+		img_icon.src = "images/icon_processing.gif";
+		td_icon.appendChild(img_icon);
+		tr.appendChild(td_icon);
+
+		StatusNet.debug('x2');
+		var td_name = document.createElement('td');
+		$(td_name).addClass('name').text(acct.username);
+		tr.appendChild(td_name);
+
+		StatusNet.debug('x3');
+		var td_site = document.createElement('td');
+		$(td_site).addClass('site').text(acct.apiroot);
+		tr.appendChild(td_site);
+		
+		StatusNet.debug('x4');
+		list.appendChild(tr);
+		
+		acct.fetchUrl('account/verify_credentials.xml', function(status, xml) {
+			var avatar = $("user profile_image_url", xml).text();
+			StatusNet.debug(avatar);
+			img_icon.src = avatar;
+		}, function(status) {
+			StatusNet.debug("We failed to load account info");
+		});
+		
+		StatusNet.debug('bye');
+	}
 }
