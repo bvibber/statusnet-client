ccb3d1e34c1297774c4c291a1ea442c23b647683
Brion Vibber <brion@pobox.com>
2010-05-18 15:38:04 -0700

    work in progress: new mobile ui frontend

--- StatusNet Mobile/Resources/app.js
+++ StatusNet Mobile/Resources/app.js
@@ -3,6 +3,7 @@
 // this sets the background color of the master UIView (when there are no windows/tab groups on it)
 Titanium.UI.setBackgroundColor('#000');
 
+/*
 var label = Titanium.UI.createLabel({
 	color:'#999',
 	text:'StatusNet Mobile',
@@ -10,12 +11,46 @@ var label = Titanium.UI.createLabel({
 	textAlign:'center',
 	width:'auto'
 });
+*/
 
-var win = Titanium.UI.createWindow();
-var webview = Titanium.UI.createWebView({url: 'index.html'});
-win.add(label);
-win.add(webview);
-win.open();
+//var win = Titanium.UI.createWindow();
+//win.open();
 
 
+// Home tab -- account setup
+var homeWindow = Titanium.UI.createWindow({
+    url:'home.js'
+});
+var homeTab = Titanium.UI.createTab({
+    icon:'images/tabs/home.png',
+    title:'Home',
+    window:homeWindow
+});
 
+// Home tab -- account setup
+var friendsWindow = Titanium.UI.createWindow({
+    url:'home.js'
+});
+var friendsTab = Titanium.UI.createTab({
+    icon:'images/tabs/home.png',
+    title:'Friends',
+    window:homeWindow
+});
+
+var repliesWindow = Titanium.UI.createWindow({
+    url:'home.js'
+});
+var repliesTab = Titanium.UI.createTab({
+    icon:'images/tabs/home.png',
+    title:'Replies',
+    window:repliesWindow
+});
+
+
+
+var tabGroup = Titanium.UI.createTabGroup();
+tabGroup.addTab(homeTab);
+tabGroup.addTab(friendsTab);
+tabGroup.addTab(repliesTab);
+tabGroup.setActiveTab(1);
+tabGroup.open();
