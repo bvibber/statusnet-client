b354d719b1a7d942797e67d61d3a74902807c036
Brion Vibber <brion@pobox.com>
2010-05-24 08:45:45 -0700

    fulls set of tabs (non-functional)

--- StatusNet Mobile/Resources/app.js
+++ StatusNet Mobile/Resources/app.js
@@ -3,54 +3,31 @@
 // this sets the background color of the master UIView (when there are no windows/tab groups on it)
 Titanium.UI.setBackgroundColor('#000');
 
-/*
-var label = Titanium.UI.createLabel({
-	color:'#999',
-	text:'StatusNet Mobile',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-*/
-
-//var win = Titanium.UI.createWindow();
-//win.open();
-
-
-// Home tab -- account setup
-var homeWindow = Titanium.UI.createWindow({
-    url:'home.js'
-});
-var homeTab = Titanium.UI.createTab({
-    icon:'images/tabs/home.png',
-    title:'Home',
-    window:homeWindow
-});
-
-// Home tab -- account setup
-var friendsWindow = Titanium.UI.createWindow({
-    url:'home.js'
-});
-var friendsTab = Titanium.UI.createTab({
-    icon:'images/tabs/home.png',
-    title:'Friends',
-    window:homeWindow
-});
-
-var repliesWindow = Titanium.UI.createWindow({
-    url:'home.js'
-});
-var repliesTab = Titanium.UI.createTab({
-    icon:'images/tabs/home.png',
-    title:'Replies',
-    window:repliesWindow
-});
-
-
-
+// For now let's stick with the same tabs we have on the desktop sidebar
+var tabNames = ['public',
+                'personal',
+                'profile',
+                'replies',
+                'favorites',
+                'inbox',
+                'search',
+                'settings'];
+var tabs = {};
+var windows = {};
 var tabGroup = Titanium.UI.createTabGroup();
-tabGroup.addTab(homeTab);
-tabGroup.addTab(friendsTab);
-tabGroup.addTab(repliesTab);
+
+for (var i = 0; i < tabNames.length; i++) {
+    var tab = tabNames[i];
+    windows[tab] = Titanium.UI.createWindow({
+        url: tab + '.js'
+    });
+    tabs[tab] = Titanium.UI.createTab({
+        icon: 'images/tabs/' + tab + '.png',
+        title: tab,
+        window: windows[tab]
+    });
+    tabGroup.addTab(tabs[tab]);
+}
+
 tabGroup.setActiveTab(1);
 tabGroup.open();
