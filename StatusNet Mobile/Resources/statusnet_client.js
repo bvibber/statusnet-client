a2f1dfd636c600e58a4ba74833373546258f0975
Zach Copley <zach@status.net>
2010-08-23 18:55:15 -0700

    Stub out inbox and search until we get these working.

--- StatusNet Mobile/Resources/statusnet_client.js
+++ StatusNet Mobile/Resources/statusnet_client.js
@@ -374,9 +374,9 @@ StatusNet.Client.prototype.initAccountView = function(acct) {
             'friends': {deselectedImage: 'images/tabs/new/friends.png', selectedImage: 'images/tabs/new/friends_on.png', name: 'friends'},
             'mentions': {deselectedImage: 'images/tabs/new/mentions.png', selectedImage: 'images/tabs/new/mentions_on.png', name: 'mentions'},
             'profile': {deselectedImage: 'images/tabs/new/profile.png', selectedImage: 'images/tabs/new/profile_on.png', name: 'user'},
-            'favorites': {deselectedImage: 'images/tabs/new/favorites.png', selectedImage: 'images/tabs/new/favorites_on.png', name: 'favorites'},
-            'inbox': {deselectedImage: 'images/tabs/new/inbox.png', selectedImage: 'images/tabs/new/inbox_on.png', name: 'inbox'},
-            'search': {deselectedImage: 'images/tabs/new/search.png', selectedImage: 'images/tabs/new/search_on.png', name: 'search'}
+            'favorites': {deselectedImage: 'images/tabs/new/favorites.png', selectedImage: 'images/tabs/new/favorites_on.png', name: 'favorites'}
+          //  'inbox': {deselectedImage: 'images/tabs/new/inbox.png', selectedImage: 'images/tabs/new/inbox_on.png', name: 'inbox'},
+    //        'search': {deselectedImage: 'images/tabs/new/search.png', selectedImage: 'images/tabs/new/search_on.png', name: 'search'}
         };
 
         this.toolbar = StatusNet.createTabbedBar(tabinfo, this.mainwin, 1);
