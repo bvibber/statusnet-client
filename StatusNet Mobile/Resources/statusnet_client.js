5b24372cf70d7abd4d0e9271af8f3c109321b0fe
Brion Vibber <brion@pobox.com>
2010-08-27 17:56:52 -0700

    A little extra debug info in case the account init esplodes again

--- StatusNet Mobile/Resources/statusnet_client.js
+++ StatusNet Mobile/Resources/statusnet_client.js
@@ -403,8 +403,11 @@ StatusNet.Client.prototype.initAccountView = function(acct) {
         // safe to start work...
         this.webViewReady = false;
         var onReady = function() {
+            StatusNet.debug('initAccountView (delayed) triggered!');
             Titanium.App.removeEventListener('StatusNet_timelineReady', onReady);
+            StatusNet.debug('initAccountView (delayed) removed listener');
             that.webViewReady = true;
+            StatusNet.debug('initAccountView triggering timeline setup...');
             that.switchView('friends');
             StatusNet.debug('initAccountView (delayed) done.');
         };
