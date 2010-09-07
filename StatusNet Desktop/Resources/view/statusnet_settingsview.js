a4fe170991078cbc51d9d1c78eaa5921289345aa
Zach Copley <zach@status.net>
2010-09-06 23:50:55 -0700

    Add tabbed navigation to settings panel

--- StatusNet Desktop/Resources/view/statusnet_settingsview.js
+++ StatusNet Desktop/Resources/view/statusnet_settingsview.js
@@ -65,6 +65,19 @@ StatusNet.SettingsView.prototype.init = function() {
     $("#new-cancel").click(function() {
         that.hideAddAccount();
     });
+
+    // tabbed menu stuff
+    $(".tab_content").hide();
+    $("ul.tabs li:first").addClass("active").show();
+    $(".tab_content:first").show();
+    $("ul.tabs li").click(function() {
+        $("ul.tabs li").removeClass("active");
+        $(this).addClass("active");
+        $(".tab_content").hide();
+        var activeTab = $(this).find("a").attr("href");
+        $(activeTab).fadeIn();
+        return false;
+    });
 };
 
 StatusNet.SettingsView.prototype.formComplete = function() {
