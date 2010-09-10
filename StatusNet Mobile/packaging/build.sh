93aad613234619bf85a1c645428685f23dab9aa4
Brion Vibber <brion@pobox.com>
2010-09-10 02:09:27 -0700

    Fix for build scripts to tweak file metadata correctly (preserve internal symlink in app bundle)

--- StatusNet Mobile/packaging/build.sh
+++ StatusNet Mobile/packaging/build.sh
@@ -2,7 +2,7 @@ chmod a+r *.mobileprovision && \
 if [ -e 'Payload' ]; then rm -rf Payload; fi && \
 if [ -e 'iTunesArtwork' ]; then rm -rf iTunesArtwork; fi && \
 mkdir 'Payload' && \
-cp -pr 'StatusNet Mobile.app' 'Payload/StatusNet Mobile.app' && \
+cp -pR 'StatusNet Mobile.app' 'Payload/StatusNet Mobile.app' && \
 cp -f 'iTunesArtwork.jpg' 'iTunesArtwork' && \
-zip -r 'StatusNet Mobile.ipa' 'Payload' 'iTunesArtwork' && \
+zip --symlinks -r 'StatusNet_Mobile.ipa' 'Payload' 'iTunesArtwork' && \
 cp -p app.apk 'StatusNet Mobile.apk'
