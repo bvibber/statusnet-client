1053b10c190a87fa1af24fd7ddac8f67893efed4
Brion Vibber <brion@pobox.com>
2010-09-15 16:41:02 -0700

    Update to packaging scripts, including Android signing helper script. Currently these have some hardcoded paths for my dev workstation, will clean up later. ;)

--- StatusNet Mobile/packaging/build.sh
+++
@@ -1,8 +0,0 @@
-chmod a+r *.mobileprovision && \
-if [ -e 'Payload' ]; then rm -rf Payload; fi && \
-if [ -e 'iTunesArtwork' ]; then rm -rf iTunesArtwork; fi && \
-mkdir 'Payload' && \
-cp -pR 'StatusNet Mobile.app' 'Payload/StatusNet Mobile.app' && \
-cp -f 'iTunesArtwork.jpg' 'iTunesArtwork' && \
-zip --symlinks -r 'StatusNet_Mobile.ipa' 'Payload' 'iTunesArtwork' && \
-cp -p app.apk 'StatusNet Mobile.apk'
